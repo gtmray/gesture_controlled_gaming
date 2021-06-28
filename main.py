e83fc777cc69aca9b41aae632305edbc431b7543
Rewan Gautam <uni.rayone@gmail.com>
2021-06-28 18:38:00 +0545

    some testing config

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pydirectinput as pd
 
 model = 'models\\model_new_2.h5'
 model = load_model(model)
-pd.PAUSE = 0.01  # Default is 0.1 so to avoid lagging
+pd.PAUSE = 0.001  # Default is 0.1 so to avoid lagging
 
 
 def preprocess_img(img_for_pred):
@@ -22,6 +22,7 @@ def preprocess_img(img_for_pred):
     img_pred = np.reshape(img_new, [1, img_width, img_height, 3])  # Reshaping to shape that keras accepts
     img_pred = preprocess_input(img_pred)
     return img_pred
+
 
 cap = cv2.VideoCapture(1)
 
@@ -43,6 +44,7 @@ obj_hand = ObjectDetection(draw_contour=1, hands_or_blue=0, p1=p1_hand, p2=p2_ha
 
 while True:
     _, img = cap.read()
+    #img = cv2.resize(img, (800, 700))
     img = cv2.flip(img, 2)
     img_pre_hand, img_hand = obj_hand.preprocess(img)  # Get preprocessed image
     img_contour_hand, _ = obj_hand.contours(img_pre_hand, img_hand)  # Get image with contours
@@ -72,7 +74,7 @@ while True:
         pd.press('Space')
 
     cv2.putText(img_contour_blue, text, (300, 90), cv2.FONT_HERSHEY_SIMPLEX, 3, (150, 75, 94), 2, cv2.LINE_AA)
-    cv2.imshow('Hands', img_pre_hand) #  ROI hands image
+    cv2.imshow('Hands', img_pre_hand)  # ROI hands image
     cv2.imshow('Blue', img_pre_blue)
     cv2.imshow('Image', img_contour_blue)
 
